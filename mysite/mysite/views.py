ab9f62f133b1dd356cd6a3373f63f5eb4debf141
albertjone <guanalbertjone@gmail.com>
2018-03-30 17:59:01 +0800

    Chapter4

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,19 +1,33 @@
+from django.template.loader import get_template
 from django.http import HttpResponse,Http404
+from django.template import Template,Context
+from django.shortcuts import render_to_response
 import datetime
 
 def hello(request):
     return HttpResponse("Hello world")
 
 def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    #now = datetime.datetime.now()
+    # html = "<html><body>It is now %s.</body></html>" % now
+
+    # t = get_template('current_datetime.html')
+    # html = t.render(Context({'current_date':now}))
+    # return HttpResponse(html)
+
+    # return render_to_response('current_date.html',{'current_date':now})
+
+    current_datetime = datetime.datetime.now()
+    return render_to_response('current_datetime.html',locals())
+
+
 
 def hours_ahead(request,offset):
-    # try: 
-    #     offset = int(offset)
-    # except ValueError:
-    #     raise Http404()
+    try: 
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
     dt = datetime.datetime.now()+datetime.timedelta(offset)
     html = "<html><body>In %s hours, it will be %s." %(offset,dt)
     return HttpResponse(html)
+    
